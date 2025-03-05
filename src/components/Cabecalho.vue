096debc59ead4415081d6673078da9f8b275670f
Ariel <ariel@example.com>
2025-03-05 17:46:44 -0300

    Ajuste de caminho da imagem

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -6,7 +6,7 @@
     <div class="row">
         <header class="col d-flex flex-column align-items-center justify-content-center">
             <br>
-            <img src="@/images/calculate.png" alt="ícone de calculadora">
+            <img src="@/images/Calculate.png" alt="ícone de calculadora">
             <br>
             <h1 class="text-center">VAMOS CALCULAR?</h1>
             <br>
